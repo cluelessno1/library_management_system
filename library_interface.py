98bdd411aa53258fb6b04d84d1e121a9bd36bfd9
Shashwat Agrawal <shashwat1999@gmail.com>
2019-07-28 00:14:53 +0530

    Basic Class Created and Executed

---
+++ library_interface.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+class book_details:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
+
+print("\t\t\t*****Library Management System*****")
+print("1. Adminsitrator \n2. Student")
+choice = input("Enter your choice: ")
+
+
+
+"""
+title = input("Enter the Title of the book: ")
+author = input("Enter the Author of the book: ")
+
+b1 = book_details(title, author)
+
+
+
+print(b1.title)
+print(b1.author)
+"""
