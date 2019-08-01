bf3d6444305dbceed530bf427c094b4ffdd6e1aa
Shashwat Agrawal <shashwat1999@gmail.com>
2019-08-02 02:03:10 +0530

    Added ability to search for files

--- databse_practice_2.py
+++ databse_practice_2.py
@@ -11,9 +11,13 @@ Base = declarative_base()
 
 class Book(Base):
    __tablename__ = 'books'
-   id = Column(Integer, primary_key=True)
+   id = Column(Integer, primary_key=True) # Every SQLAlchemy table should have a primary key named 'id'
+   isbn = Column(Integer)
    name = Column(String)
    author = Column(String)
+   publisher = Column(String)
+   quantity = Column(Integer)
+   checked_out = Column(Integer)
 
 Base.metadata.create_all(engine)
 
@@ -22,19 +26,55 @@ Session = sessionmaker(bind = engine)
 session = Session()
 
 def add_record():
+    book_isbn = input("Enter ISBN number of the book :")
     book_name = input("Enter the book name :")
     book_author = input("Enter the book author :")
-    session.add(Book(name = book_name, author = book_author))
+    book_publisher = input("Enter the publisher of the book :")
+    book_quantity = input("Enter the total no. of books being added to the library :")
+    session.add(Book(isbn = int(book_isbn), name = book_name, author = book_author, publisher = book_publisher, quantity = int(book_quantity)))
     session.commit()
     main_menu()
 
 def display_records():
-    result = session.query(Book).from_statement(text("SELECT * FROM books")).all()
-    print("\n")
-    for row in result:
-        print("Book Name: ", row.name, "Author:", row.author)
-    print("\n")
-    main_menu()
+    print("1. Display details of all books\n2. Search books by ISBN\n3. Search books by book name\n4. Search books by author\n5. Go back to main menu :")
+    choice = input("Enter you choice :")
+    if choice == '1':
+        result = session.query(Book).from_statement(text("SELECT * FROM books")).all()
+        print("\n")
+        for row in result:
+            print("ISBN: ", row.isbn, "Book Name: ", row.name, "Author:", row.author, "Publisher :", row.publisher, "Quantity :", row.quantity, "Checked Out :", row.checked_out)
+        print("\n")
+        main_menu()
+    elif choice == '2':
+         book_isbn = input("Enter ISBN number of the book :")
+         result = session.query(Book).filter(Book.isbn == int(book_isbn))
+         print("\n")
+         for row in result:
+            print("ISBN: ", row.isbn, "Book Name: ", row.name, "Author:", row.author, "Publisher :", row.publisher, "Quantity :", row.quantity, "Checked Out :", row.checked_out)
+         print("\n")
+         main_menu()
+    elif choice == '3':
+        book_name = input("Enter title of the book :")
+        result = session.query(Book).filter(Book.name == book_name)
+        print("\n")
+        for row in result:
+            print("ISBN: ", row.isbn, "Book Name: ", row.name, "Author:", row.author, "Publisher :", row.publisher, "Quantity :", row.quantity, "Checked Out :", row.checked_out)
+        print("\n")
+        main_menu()
+    elif choice == '4':
+        book_author = input("Enter author of the book :")
+        result = session.query(Book).filter(Book.author == book_author)
+        print("\n")
+        for row in result:
+            print("ISBN: ", row.isbn, "Book Name: ", row.name, "Author:", row.author, "Publisher :", row.publisher, "Quantity :", row.quantity, "Checked Out :", row.checked_out)
+        print("\n")
+        main_menu()
+    elif choice == '5':
+        main_menu()
+    else:
+        print("Entered invaild input. Please enter the input again.")
+        display_records()
+
 
 def main_menu():
 
