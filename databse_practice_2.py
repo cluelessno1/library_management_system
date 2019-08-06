8c01e4c6d286a0bf6c211deb4cbb8b17f29981ad
Shashwat Agrawal <shashwat1999@gmail.com>
2019-08-07 01:49:48 +0530

    Added a function to only allow unique and appropriate ISBN values.

--- databse_practice_2.py
+++ databse_practice_2.py
@@ -25,8 +25,45 @@ from sqlalchemy.orm import sessionmaker
 Session = sessionmaker(bind = engine)
 session = Session()
 
-def add_record():
+
+def update_record(book_isbn):
+    print("Have to make this")
+    main_menu()
+
+def check_isbn(): ##To check if the length of the inputted ISBN is 10 or 13 and whther such an ISBN already exists.
     book_isbn = input("Enter ISBN number of the book :")
+    if len(book_isbn)!=10:
+        if len(book_isbn)!=13:
+            print("ISBN is 10 or 13 digit number, please enter it correctly again")
+            print(book_isbn)
+            check_isbn()
+    result = session.query(Book).all()
+    flag = False
+    for row in result:
+        if row.isbn == int(book_isbn):
+            flag = True
+            break
+    if flag: #If written while flag: it gave logical error on execution. The value of book_isbn became fixed at the first eneterd value,
+        print("The record with this ISBN already exists.\nEnter a new record or modify data of existing record.")
+        while True:
+            choice = int(input("1. Enter new record\n2. Modify existing record\n3. Main Menu\n Enter your choice: "))
+            if choice == 1:
+                check_isbn()
+                break
+            elif choice == 2:
+                update_record(int(book_isbn)) #Have to create this function.
+                break
+            elif choice == 3:
+                main_menu()
+                break
+            else:
+                print("Entered invalid option.\nPlease enter once again.")
+
+    return book_isbn
+
+
+def add_record():
+    book_isbn = check_isbn()
     book_name = input("Enter the book name :")
     book_author = input("Enter the book author :")
     book_publisher = input("Enter the publisher of the book :")
@@ -80,7 +117,14 @@ def main_menu():
 
     print("####### Main Menu #########")
     print()
-    choice = input("1. Add a record\n2. Display all records\n3. Exit\nEnter your choice:")
+    choice = input("1. Add record\n2. Display record\n3. Exit\nEnter your choice:")
+
+    # Add a delete record and update record to complete the first part
+    # After that try and create a table members/memberships and try to integrate that with the table Book
+    # Try and add an interface to borrow books(change Checked Out value)
+    # After all that is done, learn how to navigate to different windows in Tkinter
+    # After that try and migrate your database system to GUI
+
     if choice == '1': ###The input is being taken as a string, if we write 1 insted of '1', it will not work.
         add_record()
     elif choice == '2':
