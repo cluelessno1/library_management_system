4f18aec1203c31020dbe1d856a418afde59bd70f
Shashwat Agrawal <shashwat1999@gmail.com>
2019-08-15 01:02:27 +0530

    Delete function added and polished it up a bit. One table operations completed.

--- databse_practice_2.py
+++ databse_practice_2.py
@@ -108,6 +108,57 @@ def update_record():
         update_record()
 
 
+def delete_record():
+    choice = int(input("1. Delete Record\n2. Main Menu"))
+    if choice == 1:
+        while True:
+            book_isbn = int(input("Enter the ISBN Number of book who's data you want to delete :"))
+            if len(str(book_isbn)) != 10:
+                if len(str(book_isbn)) != 13:
+                    print("ISBN is 10 or 13 digit number, please enter it correctly again")
+                    continue
+            result = session.query(Book).all()
+            flag = False
+            for row in result:
+                if row.isbn == int(book_isbn):
+                    flag = True
+                    break
+            if flag:  # If written while flag: it gave logical error on execution. The value of book_isbn became fixed at the first eneterd value,
+                break
+            else:
+                print("\n\nThis ISBN doesn't exist.\nEnter an existing ISBN number to delete record.\n\n")
+                continue
+        # Have taken a valid ISBN, will display it now and ask the user if he really wants to delete it.
+        result = session.query(Book).filter(Book.isbn == int(book_isbn))
+        print("\n")
+        for row in result:
+            print("ISBN: ", row.isbn, "Book Name: ", row.name, "Author:", row.author, "Publisher :", row.publisher,
+                  "Quantity :", row.quantity, "Checked Out :", row.checked_out)
+        print("\n")
+
+        while True:
+            # x = session.query(Book).filter(Book.isbn == int(book_isbn))
+            choice_1 = int(input("Do you really want to delete the record\n1. Yes\n2. No\nEnter your choice :"))
+            if choice_1 == 1:
+                for row in result:  # It didn't work if directly writing result instead of using a for loop and writing row
+                    session.delete(row)
+                    session.commit()
+                print("The record with ISBN "+str(book_isbn)+" has been successfully deleted.")
+                break
+            elif choice_1 == 2:
+                print("The record wasn't deleted. You're being returned to the main menu.")
+                break
+            else:
+                print("Invalid input\nPlease enter again")
+                continue
+        main_menu()
+    elif choice == 2:
+        main_menu()
+    else:
+        print("Invalid choice enterred, please enter again")
+        delete_record()
+
+
 def check_isbn(): ##To check if the length of the inputted ISBN is 10 or 13 and whther such an ISBN already exists.
     book_isbn = input("Enter ISBN number of the book :")
     if len(book_isbn)!=10:
@@ -155,34 +206,53 @@ def display_records():
     choice = input("Enter you choice :")
     if choice == '1':
         result = session.query(Book).from_statement(text("SELECT * FROM books")).all()
+        x = 0
         print("\n")
         for row in result:
-            print("ISBN: ", row.isbn, "Book Name: ", row.name, "Author:", row.author, "Publisher :", row.publisher, "Quantity :", row.quantity, "Checked Out :", row.checked_out)
+            x = x + 1
+            print("ISBN: ", row.isbn, "Book Name: ", row.name, "Author:", row.author, "Publisher :", row.publisher,
+                  "Quantity :", row.quantity, "Checked Out :", row.checked_out)
         print("\n")
+        if x == 0:
+            print("No record exists in the database.")
         main_menu()
     elif choice == '2':
          book_isbn = input("Enter ISBN number of the book :")
          result = session.query(Book).filter(Book.isbn == int(book_isbn))
+         x = 0
          print("\n")
          for row in result:
-            print("ISBN: ", row.isbn, "Book Name: ", row.name, "Author:", row.author, "Publisher :", row.publisher, "Quantity :", row.quantity, "Checked Out :", row.checked_out)
+             x = x + 1
+             print("ISBN: ", row.isbn, "Book Name: ", row.name, "Author:", row.author, "Publisher :", row.publisher, "Quantity :", row.quantity, "Checked Out :", row.checked_out)
          print("\n")
+         if x == 0:
+             print("The entered ISBN doesn't exist in the database.")
          main_menu()
     elif choice == '3':
         book_name = input("Enter title of the book :")
         result = session.query(Book).filter(Book.name == book_name)
+        x = 0
         print("\n")
         for row in result:
-            print("ISBN: ", row.isbn, "Book Name: ", row.name, "Author:", row.author, "Publisher :", row.publisher, "Quantity :", row.quantity, "Checked Out :", row.checked_out)
+            x = x + 1
+            print("ISBN: ", row.isbn, "Book Name: ", row.name, "Author:", row.author, "Publisher :", row.publisher,
+                  "Quantity :", row.quantity, "Checked Out :", row.checked_out)
         print("\n")
+        if x == 0:
+            print("The entered book name doesn't exist in the database.")
         main_menu()
     elif choice == '4':
         book_author = input("Enter author of the book :")
         result = session.query(Book).filter(Book.author == book_author)
+        x = 0
         print("\n")
         for row in result:
-            print("ISBN: ", row.isbn, "Book Name: ", row.name, "Author:", row.author, "Publisher :", row.publisher, "Quantity :", row.quantity, "Checked Out :", row.checked_out)
+            x = x + 1
+            print("ISBN: ", row.isbn, "Book Name: ", row.name, "Author:", row.author, "Publisher :", row.publisher,
+                  "Quantity :", row.quantity, "Checked Out :", row.checked_out)
         print("\n")
+        if x == 0:
+            print("The entered book author doesn't exist in the database.")
         main_menu()
     elif choice == '5':
         main_menu()
@@ -195,7 +265,7 @@ def main_menu():
 
     print("####### Main Menu #########")
     print()
-    choice = input("1. Add record\n2. Update Record \n3. Display record\n4. Exit\nEnter your choice:")
+    choice = input("1. Add record\n2. Update Record \n3. Display record\n4. Delete recoed\n5. Exit\nEnter your choice:")
 
     # Add a delete record and update record to complete the first part
     # After that try and create a table members/memberships and try to integrate that with the table Book
@@ -210,9 +280,11 @@ def main_menu():
     elif choice == '3':
         display_records()
     elif choice == '4':
+        delete_record()
+    elif choice == '5':
         sys.exit()
     else:
-        print("The valid inputs are 1,2 and 3 only.\nPlease try again.")
+        print("The valid inputs are 1,2,3,4 and 5 only.\nPlease try again.")
         main_menu()
 
 main_menu()
